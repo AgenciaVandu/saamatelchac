0f2d3ee07f92677ae7fa9267f645405ded1cd4b5
Alvar Buenfil <vandu@MacBook-Air-de-Alvar.local>
2025-09-17 13:07:13 -0600

    galeria

--- resources/views/components/galeria.blade.php
+++ resources/views/components/galeria.blade.php
@@ -9,7 +9,25 @@
         <div class="item">
             <img src="{{asset('img/amenidad.webp')}}" alt="Amenidad saama telchac">
         </div>
-
+        <div class="item">
+            <img src="{{asset('img/webp/amenidad-1.webp')}}" alt="Amenidad saama telchac">
+        </div>
+        <div class="item">
+            <img src="{{asset('img/webp/amenidad-2.webp')}}" alt="Amenidad saama telchac">
+        </div>
+        <div class="item">
+            <img src="{{asset('img/webp/amenidad-3.webp')}}" alt="Amenidad saama telchac">
+        </div>
+        <div class="item">
+            <img src="{{asset('img/webp/amenidad-4.webp')}}" alt="Amenidad saama telchac">
+        </div>
+        <div class="item">
+            <img src="{{asset('img/webp/amenidad-5.webp')}}" alt="Amenidad saama telchac">
+        </div>
+        <div class="item">
+            <img src="{{asset('img/webp/amenidad-6.webp')}}" alt="Amenidad saama telchac">
+        </div>
+        
     </div>
     <div class="carrusel">
         <div class="item">
@@ -21,6 +39,25 @@
         <div class="item">
             <img src="{{asset('img/amenidad.webp')}}" alt="Amenidad saama telchac">
         </div>
+        <div class="item">
+            <img src="{{asset('img/webp/amenidad-1.webp')}}" alt="Amenidad saama telchac">
+        </div>
+        <div class="item">
+            <img src="{{asset('img/webp/amenidad-2.webp')}}" alt="Amenidad saama telchac">
+        </div>
+        <div class="item">
+            <img src="{{asset('img/webp/amenidad-3.webp')}}" alt="Amenidad saama telchac">
+        </div>
+        <div class="item">
+            <img src="{{asset('img/webp/amenidad-4.webp')}}" alt="Amenidad saama telchac">
+        </div>
+        <div class="item">
+            <img src="{{asset('img/webp/amenidad-5.webp')}}" alt="Amenidad saama telchac">
+        </div>
+        <div class="item">
+            <img src="{{asset('img/webp/amenidad-6.webp')}}" alt="Amenidad saama telchac">
+        </div>
+       
     </div>
 
 </section>
